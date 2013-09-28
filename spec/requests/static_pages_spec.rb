07b6f4b24bb15b5a92909cc9d6475e8dfec6398e
rubykaikean <khongkk89@gmail.com>
2013-09-28 14:38:40 +0800

    finish layout routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,41 +5,35 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "home Page" do
-    it "should have the content 'Sample App'" do
+    before { visit root_path}
+    subject { page }
+
+    it  { should have_content ('Sample App')}
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
-
+    it { should have_title ("Ruby on Rails Tutorial Sample App | Home") }
   end
 
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe "About page" do
   	it "should have the content 'About Us'"do
-  	  visit '/static_pages/about'
+  	  visit about_path
   	  expect(page).to have_content("About Us")
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
@@ -59,6 +53,19 @@ describe "StaticPages" do
   #   it "should not have a custom page title" do
   #     visit '/static_pages/home'
   #     expect(page).not_to have_title('| Home')
+  #   end
+  # end
+
+  # describe "Contact page" do
+
+  #   it "should have the content 'Contact'" do
+  #     visit contact_path
+  #     expect(page).to have_content('Contact')
+  #   end
+
+  #   it "should have the title 'Contact'" do
+  #     visit contact_path
+  #     expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
   #   end
   # end
 
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,18 @@
+
+# cmd > rails g integration_test user_pages >>> generate file
+
 require 'spec_helper'
 
-describe "Users" do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get users_path
-      response.status.should be(200)
-    end
+describe "UserPages" do 
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
+  
+
 end
